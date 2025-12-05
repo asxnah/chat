1213e8d3579f35a0889e0af25821a4246763f6c9
⋆｡˚꒰ঌ 아수나 ໒꒱˚｡⋆ <cherepanova.so04@gmail.com>
2025-12-05 10:29:59 +0500

    init

---
+++ app/store/rootReducer.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import exampleReducer from "@/store/slices/exampleSlice";
+
+export const rootReducer = combineReducers({
+  example: exampleReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
