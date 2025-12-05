15d0160d2a27fb1968f623729b70778d9af102d9
⋆｡˚꒰ঌ 아수나 ໒꒱˚｡⋆ <cherepanova.so04@gmail.com>
2025-12-05 14:32:48 +0500

    chore: popup component

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import { Confirm } from "@/ui/Confirm";
 import { Input } from "@/ui/Input";
 import { Toggler } from "@/ui/Toggler";
 import { Form } from "@/widgets/Form";
+import { Popup } from "@/widgets/Popup";
 
 export default function Home() {
   const [value, setValue] = useState<string>("");
   const [checked, setChecked] = useState<boolean>(false);
+  const [popupShown, setPopupShown] = useState<boolean>(false);
 
   return (
     <main className="p-4 flex flex-col gap-2">
@@ -41,6 +43,20 @@ export default function Home() {
         buttonText="Submit"
         onSubmit={(userInfo) => console.log(userInfo)}
       />
+
+      <button
+        className="underline my-4 cursor-pointer"
+        onClick={() => setPopupShown(true)}
+      >
+        Click to open popup
+      </button>
+      {popupShown && (
+        <Popup
+          heading="Popup"
+          children={<p>Hello, World!</p>}
+          onClose={() => setPopupShown(false)}
+        />
+      )}
     </main>
   );
 }
---
+++ app/widgets/Popup/Popup.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react";
+import { X } from "lucide-react";
+
+interface PopupProps {
+  heading: string;
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export const Popup = ({ heading, children, onClose }: PopupProps) => {
+  return (
+    <section className="absolute top-0 left-0 z-10 w-full h-full grid place-content-center bg-(--screen-dim)">
+      <div className="relative p-8 w-[32rem] flex flex-col gap-8 rounded-lg bg-(--white)">
+        <div className="flex items-center justify-between">
+          <h3 className="text-(--black) text-xl font-semibold">{heading}</h3>
+          <button className="cursor-pointer" type="button" onClick={onClose}>
+            <X className="stroke-(--lightgrey)" />
+          </button>
+        </div>
+        {children}
+      </div>
+    </section>
+  );
+};
