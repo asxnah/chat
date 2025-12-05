5dad68f7cf9ccc4cc21d922f4c24ba6f074d95e4
⋆｡˚꒰ঌ 아수나 ໒꒱˚｡⋆ <cherepanova.so04@gmail.com>
2025-12-05 16:57:35 +0500

    chore: tabbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/ui/Input";
 import { Toggler } from "@/ui/Toggler";
 import { Form } from "@/widgets/Form";
 import { Popup } from "@/widgets/Popup";
+import { TabBar } from "./widgets/TabBar";
 
 export default function Home() {
   const [value, setValue] = useState<string>("");
@@ -15,48 +16,46 @@ export default function Home() {
   const [popupShown, setPopupShown] = useState<boolean>(false);
 
   return (
-    <main className="p-4 flex flex-col gap-2">
-      <Button content="Test" onClick={() => console.log("Click")} />
-
-      <div className="w-full p-4 bg-neutral-200">
-        <Confirm
-          content="Confirm?"
-          submit={() => console.log("Submitted")}
-          onDecline={() => console.log("Declined")}
+    <>
+      <TabBar />
+      <main className="p-4 flex flex-col gap-2">
+        <Button content="Test" onClick={() => console.log("Click")} />
+        <div className="w-full p-4 bg-neutral-200">
+          <Confirm
+            content="Confirm?"
+            submit={() => console.log("Submitted")}
+            onDecline={() => console.log("Declined")}
+          />
+        </div>
+        <Input
+          id="example"
+          placeholder="Example"
+          value={value}
+          onChange={setValue}
         />
-      </div>
-
-      <Input
-        id="example"
-        placeholder="Example"
-        value={value}
-        onChange={setValue}
-      />
-
-      <Toggler
-        content="Toggler"
-        checked={checked}
-        onToggle={() => setChecked((prev) => !prev)}
-      />
-
-      <Form
-        buttonText="Submit"
-        onSubmit={(userInfo) => console.log(userInfo)}
-      />
-
-      <button
-        className="underline my-4 cursor-pointer"
-        onClick={() => setPopupShown(true)}
-      >
-        Click to open popup
-      </button>
-      {popupShown && (
-        <Popup
-          heading="Popup"
-          children={<p>Hello, World!</p>}
-          onClose={() => setPopupShown(false)}
+        <Toggler
+          content="Toggler"
+          checked={checked}
+          onToggle={() => setChecked((prev) => !prev)}
         />
-      )}
-    </main>
+        <Form
+          buttonText="Submit"
+          onSubmit={(userInfo) => console.log(userInfo)}
+        />
+        <button
+          className="underline my-4 cursor-pointer"
+          onClick={() => setPopupShown(true)}
+        >
+          Click to open popup
+        </button>
+        {popupShown && (
+          <Popup
+            heading="Popup"
+            children={<p>Hello, World!</p>}
+            onClose={() => setPopupShown(false)}
+          />
+        )}
+      </main>
+    </>
   );
 }
