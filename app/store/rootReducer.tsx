85df8fdb60be3e974cfdbcf61c92921a94ce6ff9
asuna <cherepanova.so04@gmail.com>
2025-12-06 16:29:51 +0500

    chore: path & single quotes

--- app/store/rootReducer.tsx
+++ app/store/rootReducer.tsx
@@ -1,8 +1,8 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import exampleReducer from "@/store/slices/exampleSlice";
+import { combineReducers } from '@reduxjs/toolkit';
+import exampleReducer from '@/store/slices/exampleSlice';
 
 export const rootReducer = combineReducers({
-  example: exampleReducer,
+	example: exampleReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
