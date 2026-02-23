85a02dada3358503cce6f930e35fbb6668d744d3
asuna <cherepanova.so04@gmail.com>
2026-02-23 21:50:06 +0500

    chore(Chat widget): add selected state & refactor variables name

--- src/widgets/Chat/Chat.tsx
+++ src/widgets/Chat/Chat.tsx
@@ -6,18 +6,20 @@ interface ChatProps {
   // Дата и время последнего сообщения в ISO формате
   datetime: string;
   // Текст последнего сообщения
-  msg: string;
+  message: string;
+  // Состояние выбранного чата
+  isSelected: boolean;
 }
 
-export const Chat = ({ name, datetime, msg }: ChatProps) => {
+export const Chat = ({ name, datetime, message, isSelected }: ChatProps) => {
   return (
     // Контейнер чата с аватаркой и информацией
-    <div className="px-8 py-3 flex gap-2.5">
+    <div
+      className={`px-8 py-3 flex gap-2.5 ${isSelected ? "bg-fill" : "bg-white"}`}
+    >
       {/* Аватар пользователя */}
       <div className="shrink-0 w-13.5 h-13.5 rounded-full grid place-items-center bg-lightgrey">
-        <p className="text-2xl font-bold text-white">
-          {name[0].toUpperCase()}
-        </p>
+        <p className="text-2xl font-bold text-white">{name[0].toUpperCase()}</p>
       </div>
 
       {/* Основной блок с именем, временем и последним сообщением */}
@@ -29,7 +31,7 @@ export const Chat = ({ name, datetime, msg }: ChatProps) => {
         </div>
 
         {/* Текст сообщения */}
-        <p className="line-clamp-1">{msg}</p>
+        <p className="line-clamp-1">{message}</p>
       </div>
     </div>
   );
