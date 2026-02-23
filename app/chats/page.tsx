0ceb8a68f87c5c3ec3ee7ffe960d2b75bf8f4e9c
asuna <cherepanova.so04@gmail.com>
2026-02-23 21:52:39 +0500

    feat: sorting chats by users name w/ user query & imitate backend chats peview set & selected chat preview state

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,38 +1,94 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { ChatPreview } from "@shared-types/chatPreview";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/rootReducer";
+import { set } from "@/store/slices/chatsPreview";
+import { AppDispatch } from "@/store";
 
 import { Input } from "@ui/Input";
 import { Button } from "@ui/Button";
 import { Chat } from "@widgets/Chat";
+
 import { SendIcon } from "./lib/SendIcon";
 import { Message } from "./ui/Message";
+import { EmptyStateNode } from "./ui/EmptyStateNode";
 
 // TODO:
-// - Реализовать фильтр поиска по chats
 // - Добавить обработку выбора чата и отображение сообщений
 // - Добавить меню чата
 
 const HAS_PARENT_BACKGROUND = false;
 
 const ChatsPage = () => {
-  // Получаем данные пользователя и список чатов из Redux store
+  const dispatch = useDispatch<AppDispatch>();
+  // Список чатов из Redux
   const messages = useSelector(
     (state: RootState) => state.currentChat.messages,
   );
+  // Превью чатов из Redux
   const chats = useSelector((state: RootState) => state.chatsPreview.chats);
+
+  // Загрузка чатов при монтировании
+  useEffect(() => {
+    // Имитация запроса к backend
+    setTimeout(() => {
+      // Кладем данные из backend в Redux
+      dispatch(
+        set([
+          {
+            chatId: "59d25a54-904d-4fb5-b1a4-6d42c3f03671",
+            user: {
+              userId: "5cd2aa7d-5e0e-4ad6-8123-5ef2695ed4ab",
+              name: "User 1",
+              isOnline: false,
+            },
+            message: {
+              messageId: "a1b2c3d4-e5f6-7890-abcd-ef1234567890",
+              text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              status: "idle",
+              createdAt: "2026-02-21T13:53:08.744046+00:00",
+            },
+          },
+          {
+            chatId: "ba1e3bb7-7acf-42da-a3b0-fb2a7aaaf013",
+            user: {
+              userId: "528c6104-37f7-41a9-9740-5dc9a1e7f020",
+              name: "User 2",
+              isOnline: true,
+            },
+            message: {
+              messageId: "98765432-10fe-dcba-9876-543210fedcba",
+              text: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
+              status: "idle",
+              createdAt: "2026-02-21T13:48:08.744202+00:00",
+            },
+          },
+        ]),
+      );
+    }, 500);
+  }, []);
+
+  useEffect(() => {
+    chats.length > 0 && setCurrentChatId(chats[0].chatId);
+  }, [chats]);
 
   // Состояние поля поиска
   const [query, setQuery] = useState<string>("");
   // Состояние сообщения
   const [message, setMessage] = useState<string>("");
-  // Состояние превью-чатов
+  // Текущий чат
+  const [currentChatId, setCurrentChatId] = useState<string>("");
 
-  // Эффект для загрузки чатов при монтировании страницы (пока пустой)
-  useEffect(() => {}, []);
+  const sortedChats = useMemo<ChatPreview[]>(() => {
+    if (!query) return chats;
+
+    return chats.filter((chat) =>
+      chat.user.name.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [chats, query]);
 
   return (
     <main className="flex">
@@ -47,29 +103,20 @@ const ChatsPage = () => {
             value={query}
             onChange={setQuery}
           />
-
-          {chats.length > 0 ? (
-            // Список чатов
+          {sortedChats.length > 0 ? ( // Список чатов
             <div className="flex flex-col grow">
-              {chats.map((chat) => (
+              {sortedChats.map((chat) => (
                 <Chat
                   key={chat.chatId}
                   name={chat.user.name}
                   datetime={chat.message.createdAt}
-                  msg={chat.message.text}
+                  message={chat.message.text}
+                  isSelected={currentChatId === chat.chatId}
                 />
               ))}
             </div>
           ) : (
-            // Пустое состояние чатов
-            <div className="h-full px-8 py-3 flex flex-col gap-8 items-center justify-center">
-              <p className="text-3xl text-center">
-                You don&rsquo;t
-                <br />
-                have chats yet
-              </p>
-              <Button content="Send your first message" />
-            </div>
+            <EmptyStateNode query={query} length={chats.length} />
           )}
         </div>
       </div>
