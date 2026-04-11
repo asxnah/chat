d0bd806cb2f6400bd18281838b63d9e549d3b7d7
asuna <cherepanova.so04@gmail.com>
2026-04-11 18:48:40 +0500

    feat(settings): edit account data

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,24 +1,37 @@
 "use client";
 
 import { useState } from "react";
-import { RootState } from "@store/rootReducer";
-import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { RootState } from "@store/rootReducer";
+import { updateUser } from "@store/slices/user";
 
 import { User } from "@widgets/User";
 import { ActionButton } from "@ui/ActionButton";
 import { Confirm } from "@ui/Confirm";
+import { Popup } from "@ui/Popup";
+import { Input } from "@ui/Input";
+import { Button } from "@ui/Button";
+
+interface FormData {
+  name: string;
+  email: string;
+}
 
 const SettingsPage = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
 
   const { name, email } = useSelector((state: RootState) => state.user.user);
 
   const [showConfirm, setShowConfirm] = useState(false);
-
-  const handleProfileClick = () => {
-    console.log("click");
-  };
+  const [showPopup, setShowPopup] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+    name: name,
+    email: email,
+  });
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -30,6 +43,15 @@ const SettingsPage = () => {
     handleLogout();
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    dispatch(updateUser({ key: "name", value: formData.name }));
+    dispatch(updateUser({ key: "email", value: formData.email }));
+
+    setShowPopup(false);
+  };
+
   return (
     <>
       <main>
@@ -37,7 +59,7 @@ const SettingsPage = () => {
           id={""}
           name={name}
           email={email}
-          onClick={handleProfileClick}
+          onClick={() => setShowPopup(true)}
           isAccount
         />
         <ul>
@@ -61,6 +83,33 @@ const SettingsPage = () => {
           Log Out
         </button>
       </main>
+
+      {showPopup && (
+        <Popup heading="Profile edit" onClose={() => setShowPopup(false)}>
+          <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-4">
+              <Input
+                id="name"
+                placeholder="Name"
+                value={formData.name}
+                onValueChange={(value) =>
+                  setFormData((prev) => ({ ...prev, name: value }))
+                }
+              />
+              <Input
+                id="email"
+                placeholder="Email"
+                value={formData.email}
+                onValueChange={(value) =>
+                  setFormData((prev) => ({ ...prev, email: value }))
+                }
+              />
+            </div>
+            <Button content="Save" type="submit" />
+          </form>
+        </Popup>
+      )}
+
       {showConfirm && (
         <Confirm
           content="Log out?"
